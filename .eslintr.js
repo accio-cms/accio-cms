bb85907aa1c270a7a123710d9d7be0ed5971faf7
Alexei Accio <alexei.accio@gmail.com>
2019-06-24 13:56:15 +0300

    init

---
+++ .eslintr.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  globals: {
+    __static: true,
+  },
+};
