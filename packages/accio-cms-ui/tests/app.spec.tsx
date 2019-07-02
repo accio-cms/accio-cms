25002e6268c05a367b79c62556b78c58faf6617c
Alexei Accio <alexei.accio@gmail.com>
2019-07-02 14:18:17 +0300

    accio-cms-ui

---
+++ packages/accio-cms-ui/tests/app.spec.tsx
@@ -0,0 +1,10 @@
+import React = require('react')
+import { create } from 'react-test-renderer'
+import App from '../src/app'
+
+test("Say my name, say my name...", () => {
+  const tree1 = create(
+    <App />
+  ).toJSON()
+  expect(tree1).toMatchSnapshot()
+})
---
+++ packages/accio-cms-ui/typings/require-context.macro.d.ts
@@ -0,0 +1,3 @@
+declare module 'require-context.macro' {
+  export default function requireContext(arg: string, bool: boolean, reg: any): any;
+}
